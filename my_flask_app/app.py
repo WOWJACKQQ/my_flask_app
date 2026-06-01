4aee7ebf6e99fa54dc3e94aa7626c1e72ecedaa9
柏鈞 <lbj122497@gmail.com>
2026-06-01 21:39:38 +0800

    第16週：首發上傳自主學習的計算機專案。

---
+++ my_flask_app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    avg = None
+    status = ""  # 新增：用來存放傳給網頁的評語
+    
+    if request.method == "POST":
+        C = int(request.form.get("chinese", 0))
+        E = int(request.form.get("english", 0))
+        M = int(request.form.get("math", 0))
+        S = int(request.form.get("chemistry", 0))
+        p = int(request.form.get("physics", 0))
+        b = int(request.form.get("biology", 0))
+        so = int(request.form.get("civics", 0))
+        g = int(request.form.get("geography", 0))
+        
+        avg = (((C + E + M) * 4) + ((S + p + b + so + g) * 2)) // 22
+        
+        # ===== 第 14 週 Jinja2 邏輯串接：由 Python 決定評語內容 =====
+        if avg >= 90:
+            status = "excellent"  # 優秀
+        elif avg >= 60:
+            status = "pass"       # 及格
+        else:
+            status = "fail"       # 需要加強
+            
+        return render_template("index.html", avg_result=avg, grade_status=status)
+        
+    return render_template("index.html", avg_result=None, grade_status=status)
+
+if __name__ == "__main__":
+    app.run(debug=True)
